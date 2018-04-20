dfb758ee2058672a8a7133c5d061c7d0aed0b3fb
Jeferson Alves <jefersonalvesf2009@gmail.com>
2018-04-19 23:32:08 -0300

    Request all posts from api

--- src/Components/ClassFeelings.js
+++ src/Components/ClassFeelings.js
@@ -1,26 +1,53 @@
 import React from 'react'
 import { Grid, Row, Col } from 'react-bootstrap'
+import SentimentPost from './SentimentPost'
+import { Button } from 'react-bootstrap'
 
 class ClassFeelings extends React.Component {
   constructor(props){
     super(props)
-    this.state = {id: this.props.id, email: this.props.email}
+    this.state = {
+      id: this.props.id,
+      email: this.props.email,
+      posts: [],
+      isLoad: false
+    }
+    this.fetchData = this.fetchData.bind(this)
   }
 
-  ComponentWillMount(){
-    //request api
+  async componentDidMount(){
+    const responseJson = await this.fetchData()
+
+    this.setState({
+      posts: responseJson.results,
+      isLoad: true
+    });
+  }
+
+  async fetchData(){
+    const response = await fetch('http://localhost:8000/api/posts/', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+      }
+    })
+
+    const responseJson = await response.json()
+    return responseJson
   }
 
   render(){
-    return(
+    return this.state.isLoad === true? (
       <Grid>
         <Row>
+          <h1>feelings about classes:</h1>
           <Col>
-            <h1>feelings about classes</h1>
+            <SentimentPost posts={this.state.posts}/>
           </Col>
         </Row>
       </Grid>
-    )
+    ):(<h1>loading ... </h1>)
   }
 }
 
