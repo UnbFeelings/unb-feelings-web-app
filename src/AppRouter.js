ccb69bdeab976a0bbe30e0c8b761c5919451e4c1
Fabio Teixeira <fabio1079@gmail.com>
2018-05-06 18:53:03 -0300

    Protected routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,20 +1,58 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import { WebDataStates } from './redux/initial-state';
 import Home from './components/pages/home/Home';
 import SignUpContainer from './components/pages/sign-up/SignUpContainer';
 import Feelings from './components/pages/feelings/Feelings';
 
-const AppRouter = () => {
+
+// Hight order component for Route. HOC -> Decorator of components
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+
+  // If user is logged, let him pass
+  if (user.state === WebDataStates.SUCCESS && user.data.token) {
+    return (
+      <Route
+        {...rest}
+        render={(props) => <Component {...props} />}
+      />
+    );
+  }
+
+  // If user if not logged. Send him to homepage
+  return (
+    <Route
+      {...rest}
+      render={(props) => <Redirect to='/' />}
+    />
+  );
+}
+
+
+const AppRouter = ({ user }) => {
   return (
     <BrowserRouter>
       <div>
         <Route exact path="/" component={Home} />
         <Route path="/sign-up" component={SignUpContainer} />
-        <Route path="/feelings" component={Feelings} />
+        <PrivateRoute
+          path="/feelings"
+          component={Feelings}
+          user={user}
+        />
       </div>
     </BrowserRouter>
   );
 }
 
-export default AppRouter;
+const mapStateToProps = ({ user }) => {
+  return {
+    user
+  }
+}
+
+const AppRouterContainer = connect(mapStateToProps)(AppRouter);
+
+export default AppRouterContainer;
