ada8cf5d064b105afb0bdc86c1a47427919b227d
Fabio Teixeira <fabio1079@gmail.com>
2018-05-08 21:47:56 -0300

    Add ServiceWorker

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
