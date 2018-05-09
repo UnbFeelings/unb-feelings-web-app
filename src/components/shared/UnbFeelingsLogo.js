ea2f71c079dddea3f6f862423dd06cc1d275f74d
Fabio Teixeira <fabio1079@gmail.com>
2018-05-08 21:38:58 -0300

    Delete unused stuff

--- src/components/shared/UnbFeelingsLogo.js
+++ src/components/shared/UnbFeelingsLogo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UnbFeelingsLogo = () => (
-  <img src="/assets/img/freeze/logo.png" alt="UnB feelings logo" />
+  <img src="/assets/img/logo.png" alt="UnB feelings logo" />
 );
 
 export default UnbFeelingsLogo;
