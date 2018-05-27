122fcae95817143aa557be4bfd07fe968e7068ca
ulysses <ulysses3353@gmail.com>
2018-05-26 22:12:31 -0300

    Passing StyleSheet
    Signed-off-by: Marcelo Cristiano <marceloabk@hotmail.com>

--- src/components/pages/mySubjectChart/MySubjectChart.js
+++ src/components/pages/mySubjectChart/MySubjectChart.js
@@ -1,54 +1,51 @@
 import React from 'react';
 import store from '../../../redux/store';
 import axios from '../../../configs/axios';
-import {Bar} from "react-chartjs-2";
-import PersonalBarChart from "./PersonalBarChart";
+import PersonalBarChart from './PersonalBarChart';
 
 class MySubjectChart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      subjects: [],
+    };
+  }
 
-    constructor(){
-	super();
-	this.state = {"posts":[],
-		     "subjects":[]}
-    } 
-    
-    fetchPosts(id){
-	axios.get('http://0.0.0.0:8000/api/posts/user/' + id + '/').then((response) => {
-	    this.setState({ posts: response.data.results });
-	    for(var cont = 0; cont<this.state.posts.length;cont = cont + 1){
-		this.fetchSubjects(this.state.posts[cont].subject)
-	    }   
-	});
-    }
+  componentDidMount() {
+    this.formatData();
+  }
 
-    fetchSubjects(id){
-	axios.get('http://0.0.0.0:8000/api/subjects/' + id + '/').then((response) => {
-	    if(!this.state.subjects.includes(response.data.name)){
-		this.setState(
-		    {
-		    subjects: [...this.state.subjects,response.data]
-		    }
-		);
-	    }
-	});
-    }
+  fetchSubjects(id) {
+    axios.get(`http://0.0.0.0:8000/api/subjects/${id}/`).then((response) => {
+      if (!this.state.subjects.includes(response.data.name)) {
+        this.setState({
+          subjects: [...this.state.subjects, response.data],
+        });
+      }
+    });
+  }
 
-    formatData(){
-	this.fetchPosts(store.getState().user.data.id);
-    }
+  formatData() {
+    this.fetchPosts(store.getState().user.data.id);
+  }
 
-    
-    componentDidMount(){
-	this.formatData()
-    }
-    
-    render(){
-	return(
-	    <div>
-		<PersonalBarChart labels={this.state.subjects} posts={this.state.posts}/>
-	    </div>
-	);
-    }
+  fetchPosts(id) {
+    axios.get(`http://0.0.0.0:8000/api/posts/user/${id}/`).then((response) => {
+      this.setState({ posts: response.data.results });
+      for (let cont = 0; cont < this.state.posts.length; cont += 1) {
+        this.fetchSubjects(this.state.posts[cont].subject);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <PersonalBarChart labels={this.state.subjects} posts={this.state.posts} />
+      </div>
+    );
+  }
 }
 
-export default MySubjectChart; 
+export default MySubjectChart;
