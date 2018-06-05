c46e7bfd2e28283bbffd6f4c170c542b0425d1ea
MatheusRich <matheusrichardt@gmail.com>
2018-06-04 22:46:46 -0300

    Improves sign up page

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -5,7 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import UnbFeelingsLogo from '../../shared/UnbFeelingsLogo';
 
 import { WebDataStates } from '../../../redux/initial-state';
