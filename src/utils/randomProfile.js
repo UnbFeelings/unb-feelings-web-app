eb77cf0361557ccc101d051d087c42b4b5b4dee6
Marcelo Augusto <mekmay@hotmail.com>
2018-07-05 09:17:52 -0300

    Fixing logic to generate collor for avatar.

--- src/utils/randomProfile.js
+++ src/utils/randomProfile.js
@@ -4,7 +4,7 @@ export const setAvatarURL = (name) => {
   // Red, Green and Blue
   const COLORS = ['700', '070', '007'];
   const avatarName = encodeURIComponent(name);
-  const ramdomIndex = Math.floor(avatarName.length % 2);
+  const ramdomIndex = Math.floor(avatarName.length % 3);
   const BASE_URL = 'https://ui-avatars.com/api/';
   const url = `${BASE_URL}?name=${avatarName}&color=fff&background=${COLORS[ramdomIndex]}`;
   return url;
